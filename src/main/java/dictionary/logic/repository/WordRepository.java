f39b777441989f2697fe3a737e7b4e0a8d1c17f7
Илья Гончар <iliagon1997@gmail.com>
2019-04-05 13:08:50 +0300

    fix

--- src/main/java/dictionary/logic/repository/WordRepository.java
+++ src/main/java/dictionary/logic/repository/WordRepository.java
@@ -20,20 +20,20 @@ public interface WordRepository extends JpaRepository<Word, Long> {
     List<Word> findBySpelling(String spelling);
 
     @Query("select w from Word w " +
-            "where w.language = :language and w.translateRelationUUID in " +
-            "                         (select w1.translateRelationUUID from Word w1 " +
+            "where w.language = :language and w.translateUUID in " +
+            "                         (select w1.translateUUID from Word w1 " +
             "                           where w1 = :word1)")
     List<Word> findTranslatedWords(Word word1, Language language);
 
     @Modifying
     @Transactional
-    @Query("update Word w set w.translateRelationUUID = null " +
+    @Query("update Word w set w.translateUUID = null " +
             "where w = :word")
     void deleteTranslateRelationFromWord(Word word);
 
     @Modifying
     @Transactional
-    @Query("update Word w set w.translateRelationUUID = :newTranslateRelationsUUID " +
-            "where w.translateRelationUUID = :oldTranslateRelationsUUID")
-    void updateTranslateRelationsUUID(String oldTranslateRelationsUUID, String newTranslateRelationsUUID);
+    @Query("update Word w set w.translateUUID = :newTranslateUUID " +
+            "where w.translateUUID = :oldTranslateUUID")
+    void updateTranslateRelationsUUID(String oldTranslateUUID, String newTranslateUUID);
 }
